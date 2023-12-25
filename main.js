3927990d73f280f5a55f4d345df3ca6fb39c2e6a
deno4908 <deno4908@gmail.com>
2023-12-25 18:49:17 +0700

    job

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function move(){
+    document.getElementById("co").style.backgroundColor="blue";
+    
+}
+function out(){
+    document.getElementById("co").style.backgroundColor="red";
+}
+function moveright1(){
+    document.getElementById("co").style.top  = "-9000px";
+}
