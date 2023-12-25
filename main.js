d6ab76ebb8e77af7f1d21c9605bb92610b4976f2
deno4908 <154745352+deno4908@users.noreply.github.com>
2023-12-25 19:02:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,5 +7,24 @@ function out(){
     document.getElementById("co").style.backgroundColor="red";
 }
 function moveright1(){
-    document.getElementById("co").style.top  = "-9000px";
+    document.getElementById("ko").style.top  = "-9000px";
+}
+function obj(){
+    document.getElementById("ko").style.backgroundColor="blue";
+}
+function ubj(){
+    document.getElementById("ko").style.backgroundColor="red";
+}
+function movetop(){
+    document.getElementById("ko").style.top  = "0px";
+}
+const div1=document.getElementById("co");
+const div2=document.getElementById("mybox");
+function onclick1(){
+    div2.style.animation="deno1 2s both"
+
+}
+function audio(){
+    let audio=new Audio('ssstik.io_1703498335028.mp3');
+    audio.play();
 }
